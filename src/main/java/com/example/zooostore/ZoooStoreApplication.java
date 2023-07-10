e71eeef36ba3af265e68bc18a56aa44200884bd9
PoliSmollie <48566379+PoliSmollyLIT@users.noreply.github.com>
2023-07-10 10:31:03 +0300

    Skeleton done

---
+++ src/main/java/com/example/zooostore/ZoooStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.zooostore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoooStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZoooStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zooostore/ZoooStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zooostore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoooStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
